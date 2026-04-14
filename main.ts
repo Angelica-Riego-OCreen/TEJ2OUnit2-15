ca0f56cdf317dfa5dbb0a1ccf39b60fa69d344c2
Angelica-Riego-OCreen <angelica.ocreen@stu.ocsb.ca>
2026-04-14 13:25:51 -0400

    Edited code once more

--- main.ts
+++ main.ts
@@ -1,8 +1,35 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Angelica
+ * Created on: Apr 2026
+ * This program is a nested loops.
 */
 
-basic.showString('Hello, World!')
+// variables
+let sprite: game.LedSprite = null
+let loopCounter1 = 0
+let loopCounter2 = 0
+
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// Press Button A, sprite moves clockwise
+input.onButtonPressed(Button.A, function () {
+    // setup
+    basic.clearScreen()
+    loopCounter1 = 0
+    loopCounter2 = 0
+    while (loopCounter1 < 4) {
+        loopCounter1++
+        sprite = game.createSprite(0, 0)
+        while (loopCounter2 < 4) {
+            sprite.move(1)
+            basic.pause(500)
+            loopCounter2++
+        }
+        sprite.turn(Direction.Right, 90)
+    }
+    sprite.delete()
+    basic.showIcon(IconNames.Happy)
+})
